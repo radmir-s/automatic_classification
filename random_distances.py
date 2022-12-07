9e588b2c4b51d5114f25456416630c4041cf082f
Radmir Sultamuratov <rsultamu@cougarnet.uh.edu>
2022-12-07 10:43:37 -0600

    now saves quantiles as well

--- random_distances.py
+++ random_distances.py
@@ -5,30 +5,36 @@ from random import sample
 from main_functions import hausdorff_quant
 import pandas as pd
 import argparse
+from collections import defaultdict
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-d', '--class-dir' , required=True)
 parser.add_argument('-i', '--ran-init',type=float, default=0.05)
-parser.add_argument('-q', '--haus-quants', nargs='*', type=float)
+parser.add_argument('-q', '--haus-quants', nargs='*', type=float, default=[0.35, 0.55 ,0.75, 0.95])
 parser.add_argument('-w', '--haus-weights', nargs='*')
 
 args = parser.parse_args()
 
-# ran_init = 0.05
-# class_dir = './classes/train/0/'
-# haus_q = [0.35, 0.55 ,0.75, 0.95]
-# haus_w = [1., 1., 1., 1.]
+# print(args)
+# raise KeyError
 
 ran_init = args.ran_init
 class_dir = args.class_dir
-haus_q = args.haus_quants if args.haus_quants else [0.35, 0.55 ,0.75, 0.95]
-haus_w = args.haus_weights if args.haus_weights else [1., 1., 1., 1.]
+haus_q = args.haus_quants
+
+if args.haus_weights is None or args.haus_weights[0] == 'auto':
+    haus_q = sorted(haus_q)
+    haus_w = np.diff(haus_q+[1.])
+elif args.haus_weights[0] == 'equal':
+    haus_w = np.ones_like(haus_q)/len(haus_q)
+else:
+    haus_w = np.array(args.haus_weights)
 
 assert len(haus_q) == len(haus_w), \
     "Quantiles and weights should have same length"
 
-matfiles = [file for file in os.listdir(class_dir) if file.endswith('.mat')]
+matfiles = [file for file in os.listdir(class_dir) if file.endswith('.mat')][:30]
 filepaths = {file.replace('.mat',''): os.path.join(class_dir, file) for file in matfiles}
 
 S = {k: loadshape(v) for k, v in filepaths.items()}
@@ -36,18 +42,29 @@ S = {k: loadshape(v) for k, v in filepaths.items()}
 ran_num = int(len(S)*ran_init)
 random_sample = sample(list(S.keys()), k=ran_num)
 
-dist = lambda x, y: np.sum(hausdorff_quant(x,y, haus_q) * np.array(haus_w))
+print(f'{len(S)}x{ran_num} distances are to be estimated')
+print('Quantles: ', *haus_q)
+print('Weights: ', *np.round(haus_w,2))
 
-data = {s:list() for s in random_sample}
-data['shapes'] = list()
+# quantles = lambda x, y: hausdorff_quant(x,y, haus_q)
+# comb_quant = lambda q: np.sum(q * haus_w)
+
+data = defaultdict(lambda: list())
+more_data = defaultdict(lambda: list())
 
 for s1 in S.keys():
     data['shapes'].append(s1)
+    more_data['shapes'].append(s1)
     for s2 in random_sample:
-        d = dist(S[s1], S[s2])
-        data[s2].append(d)
-
+        q_vals = hausdorff_quant(S[s1], S[s2], haus_q)
+        dist = np.sum(q_vals * haus_w)
+        data[s2].append(dist)
+        for q, v in zip(haus_q, q_vals):
+            more_data[f'{s2}_{q}'].append(v)
 
 df = pd.DataFrame(data)
+dfm = pd.DataFrame(more_data)
+
 cls = '_'.join(class_dir.split('/')[-2:])
 df.to_csv(f'distances_random_{cls}_{ran_init}.csv')
+dfm.to_csv(f'distances_random_{cls}_{ran_init}_more.csv')
