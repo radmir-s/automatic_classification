d45919599e4d6aa96988fc8b3dda9a83aee41392
Radmir Sultamuratov <rsultamu@cougarnet.uh.edu>
2022-12-14 10:52:08 -0600

    ++

--- eval_a2p_dist.py
+++ eval_a2p_dist.py
@@ -24,8 +24,8 @@ all_dir = args.all_dir
 proto_csv_path = args.proto_csv
 haus_q = args.haus_quants
 haus_w = args.haus_weights
-n_workers = args.num_workers if args.num_workers else (mp.cpu_count()-2)
-chunk_size = args.chunk_size if args.chunk_size else 5
+n_workers = args.num_workers if args.num_workers else int(mp.cpu_count()//2)
+chunk_size = args.chunk_size if args.chunk_size else 10
 
 if haus_q is None and haus_w is None:
     haus_q = np.linspace(0,1,11)
@@ -47,26 +47,32 @@ haus_w = np.round(haus_w,2)
 assert len(haus_q) == len(haus_w), \
     "Quantiles and weights should have same length"
 
-classes = [dir for dir in os.listdir(all_dir) if os.path.isdir(dir)]
+classes = [dir for dir in os.listdir(all_dir) if os.path.isdir(os.path.join(all_dir, dir))]
 
 # S = dict() # shape arrays
+shape_paths = dict()
 shape_class = dict()
-shape_path = dict()
 
 for cls in classes:
     class_dir = os.path.join(all_dir, cls)
     matfiles = [file for file in os.listdir(class_dir) if file.endswith('.mat')]
     for file in matfiles:
         shape = file.removesuffix('.mat').removeprefix('shape_')
-        shape_path[shape] = os.path.join(class_dir, file)
+        shape_paths[shape] = os.path.join(class_dir, file)
         shape_class[shape] = cls
 
-dfp = pd.read_csv(proto_csv_path)
-proto_paths = {shape: shp for shape, shp in zip(dfp['shape'].values, dfp.path.values)}
+arrays = {shape:loadshape(sh_path) for shape, sh_path in shape_paths.items()}
 
-pairs = tuple(product(shape_path.keys(), proto_paths.keys()))
-arrays = {shape:loadshape(sh_path) for shape, sh_path in shape_path.items()}
-arrays.update({shape:loadshape(sh_path) for shape, sh_path in proto_paths.items()})
+
+dfp = pd.read_csv(proto_csv_path)
+proto_paths = {shape: shp for shape, shp in zip(dfp['shape'].values, dfp['path'].values)}
+proto_class = {shape: cls for shape, cls in zip(dfp['shape'].values, dfp['class'].values)}
+proto_arrays = {shape:loadshape(sh_path) for shape, sh_path in proto_paths.items()}
+
+arrays.update(proto_arrays)
+shape_class.update(proto_class)
+
+pairs = tuple(product(shape_paths.keys(), proto_paths.keys()))[:100]
 array_pairs = tuple((arrays[s1], arrays[s2]) for s1, s2 in pairs)
 
 def dist(s1, s2):
