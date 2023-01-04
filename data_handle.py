878da651d47bff558b5a7510c0e6fa3e63169f1d
Radmir Sultamuratov <rsultamu@cougarnet.uh.edu>
2023-01-04 17:49:37 -0600

    fixes

--- data_handle.py
+++ data_handle.py
@@ -16,25 +16,21 @@ def loadshape(path, res = '',align=True):
     return s
 
 def unpack_a2p(csv_path):
-    df = pd.read_csv(csv_path).drop(['Unnamed: 0'],axis=1)
-    dfs = df.loc[:,['shape', 'shape_class', 'prototype', 'dist']]
-
+    dfs = pd.read_csv(csv_path).drop(['Unnamed: 0'],axis=1)
     protos = dfs.prototype.unique()
+    protos.sort()
 
     dfps = list()
     for proto in protos:
-        pcode = proto.removeprefix('shape_')
         dfp = dfs[dfs.prototype == proto]
         dfp = dfp.set_index('shape').sort_index()
-        dfp = dfp.rename(index=lambda x: x.removeprefix('shape_'))
-        dfp = dfp.rename(columns={'dist':pcode})
-        dfp = dfp.loc[:, [pcode]]
+        dfc = dfp.shape_class
+        dfp = dfp.rename(columns={'dist':proto})
+        dfp = dfp.loc[:, [proto]]
         dfps.append(dfp)
 
-    dfc = dfp.shape_class
     a2p = pd.concat(dfps, axis=1)
-
-    X = a2p.to_numpy()
+    X = a2p.values
     y = dfc.values
 
     return X, y
