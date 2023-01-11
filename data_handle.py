e469dc031a2a2cb91e311c044d789408c4fb9107
Radmir Sultamuratov <rsultamu@cougarnet.uh.edu>
2023-01-11 12:15:39 -0600

    added unpack_a2p_dq

--- data_handle.py
+++ data_handle.py
@@ -35,3 +35,25 @@ def unpack_a2p(csv_path):
 
     return X, y
 
+def unpack_a2p_dq(csv_path, diff=False):
+    df = pd.read_csv(csv_path)
+
+    if diff:
+        qcols = df.columns[df.columns.str.startswith('q')].to_list()[::-1]
+        for k in range(9):
+            df[qcols[k]] = df[qcols[k]] - df[qcols[k+1]]
+    
+    dfps = list()
+
+    for proto in df.prototype.unique():
+        dfp = df[df.prototype == proto]
+        dfp = dfp.set_index('shape').sort_index()
+        y = dfp.shape_class.values
+        dfp = dfp.rename(columns={f'q{k/10:01}':f'{proto}.q{k/10:01}' for k in range(1,11)})
+        dfp = dfp.loc[:,dfp.columns.str.startswith(str(proto))]
+        dfps.append(dfp)
+    
+    a2p = pd.concat(dfps, axis=1)
+    X = a2p.values
+    
+    return X, y
